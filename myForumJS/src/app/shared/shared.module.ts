d64789354070fd2e557c50267315a40c679bd903
TimiPashov <pashov91@gmail.com>
2024-03-19 23:30:16 +0200

    add css and html to footer and header

--- myForumJS/src/app/shared/shared.module.ts
+++ myForumJS/src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ThemesComponent } from './themes/themes.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ThemesComponent,
+    PostsComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    ThemesComponent,
+    PostsComponent
   ]
 })
 export class SharedModule { }
