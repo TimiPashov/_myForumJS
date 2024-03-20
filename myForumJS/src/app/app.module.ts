731c1d3302fdfeb65b1993aee0ad9f6779843214
TimiPashov <pashov91@gmail.com>
2024-03-20 10:24:52 +0200

    fix imports

--- myForumJS/src/app/app.module.ts
+++ myForumJS/src/app/app.module.ts
@@ -4,9 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http'
-import { ThemesModule } from './themes/themes.module';
+
 
 @NgModule({
   declarations: [
@@ -16,8 +15,6 @@ import { ThemesModule } from './themes/themes.module';
   imports: [
     BrowserModule,
     CoreModule,
-    SharedModule,
-    ThemesModule,
     HttpClientModule,
     AppRoutingModule
   ],
