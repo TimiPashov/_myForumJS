2fb37b03a5bd39ed706104bf189e526194f716fb
TimiPashov <pashov91@gmail.com>
2024-03-23 13:51:01 +0200

    add error page

--- myForumJS/src/app/app-routing.module.ts
+++ myForumJS/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/themes' }
+  { path: '', pathMatch: 'full', redirectTo: '/themes' },
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: ErrorPageComponent }
 ];
 
 @NgModule({
