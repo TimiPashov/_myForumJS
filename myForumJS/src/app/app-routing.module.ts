732e5eedc291cd9c66c9ff8ef399c64d1ab482ba
TimiPashov <pashov91@gmail.com>
2024-03-25 11:59:55 +0200

    add welcome page

--- myForumJS/src/app/app-routing.module.ts
+++ myForumJS/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { WelcomePageComponent } from './shared/welcome-page/welcome-page.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/themes' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: WelcomePageComponent },
   {
     path: 'auth',
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule)
--- myForumJS/src/app/shared/shared.module.ts
+++ myForumJS/src/app/shared/shared.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    WelcomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[LoaderComponent]
 })
