694ba6f554113937f6fa3459b75440323e2921d8
TimiPashov <pashov91@gmail.com>
2024-03-25 14:42:43 +0200

    btns hide if logged welcome page

--- myForumJS/src/app/shared/welcome-page/welcome-page.component.ts
+++ myForumJS/src/app/shared/welcome-page/welcome-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-welcome-page',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class WelcomePageComponent {
 
+  constructor(private userService: UserService) { }
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLoggedIn
+  }
 }
