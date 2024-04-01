5c250185260ad00ec8257b7cdc97ea50230a4127
TimiPashov <pashov91@gmail.com>
2024-04-01 19:19:30 +0300

    app reinit with tailwind

---
+++ myForumJS/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        customFont: ['"Fira Code"'],
+        // Add more custom font families as needed
+      },
+      boxShadow: {
+        'custom': '0 0 0.5em 0.1em rgba(0, 0, 0, 0.4)',
+        'hover': '0 0 0.5em 0.1em rgba(0, 0, 0, 0.2)'
+      },
+    },
+  },
+  plugins: [],
+}
+
