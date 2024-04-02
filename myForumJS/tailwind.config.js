04864d161eda1c523caabeac2ea167ac50637683
TimiPashov <pashov91@gmail.com>
2024-04-02 16:10:36 +0300

    add custom inset shadow for theme partial tailwind

--- myForumJS/tailwind.config.js
+++ myForumJS/tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       },
       boxShadow: {
         'custom': '0 0 0.5em 0.1em rgba(0, 0, 0, 0.4)',
-        'hover': '0 0 0.5em 0.1em rgba(0, 0, 0, 0.2)'
+        'hover': '0 0 0.5em 0.1em rgba(0, 0, 0, 0.2)',
+        'inset': '0 0 1em 0 rgba(0, 0, 0, 0.1) inset'
       },
       margin: {
         'center': '0 auto'
