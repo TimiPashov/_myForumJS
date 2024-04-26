7919e686268bd0dc670724f51088ebd2dba118e7
TimiPashov <pashov91@gmail.com>
2024-04-26 15:58:19 +0300

    add responsive

--- myForumJS/tailwind.config.js
+++ myForumJS/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
@@ -8,20 +10,26 @@ module.exports = {
         // Add more custom font families as needed
       },
       boxShadow: {
-        'custom': '0 0 0.5em 0.1em rgba(0, 0, 0, 0.4)',
-        'hover': '0 0 0.5em 0.1em rgba(0, 0, 0, 0.2)',
-        'inset': '0 0 1em 0 rgba(0, 0, 0, 0.1) inset',
-        'bottom': '0 0.1em 0.3em rgba(0, 0, 0, 0.2)'
+        custom: "0 0 0.5em 0.1em rgba(0, 0, 0, 0.4)",
+        hover: "0 0 0.5em 0.1em rgba(0, 0, 0, 0.2)",
+        inset: "0 0 1em 0 rgba(0, 0, 0, 0.1) inset",
+        bottom: "0 0.1em 0.3em rgba(0, 0, 0, 0.2)",
       },
       margin: {
-        'center': '0 auto'
+        center: "0 auto",
       },
       backgroundImage: {
-        'header-background': "url('https://unsplash.com/photos/a-robot-hand-holding-a-letter-that-says-ai-OFxYMp8VkdI')",
-        'footer-texture': "url('/img/footer-texture.png')",
+        "header-background":
+          "url('https://unsplash.com/photos/a-robot-hand-holding-a-letter-that-says-ai-OFxYMp8VkdI')",
+        "footer-texture": "url('/img/footer-texture.png')",
       },
     },
   },
-  plugins: [],
-}
-
+  plugins: [
+    // plugin(function ({ addBase }) {
+    //   addBase({
+    //     html: { fontSize: "16px" },
+    //   });
+    // }),
+  ],
+};
