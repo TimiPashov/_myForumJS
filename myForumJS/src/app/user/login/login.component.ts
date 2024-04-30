526378f70ea10e37b724d8d9208778749dc5e329
TimiPashov <pashov91@gmail.com>
2024-04-30 13:25:17 +0300

    Fix login button disabled state in login.component.html and add isLoading flag in login.component.ts

--- myForumJS/src/app/user/login/login.component.ts
+++ myForumJS/src/app/user/login/login.component.ts
@@ -10,7 +10,8 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  isLogged:boolean = false;
+  isLogged: boolean = false;
+  isLoading: boolean = false;
   errorMessage: string = '';
   constructor(
     private userService: UserService,
@@ -23,15 +24,18 @@ export class LoginComponent implements OnInit {
     }
 
     if (form.invalid) {
-      console.log('Form is invalid');
+      Object.values(form.controls).forEach((control) => {
+        control.markAsTouched();
+      });
       return;
     }
 
     const { email, password } = form.value;
-
+    this.isLoading = true;
     this.userService.login(email, password).subscribe(
       () => {
         this.router.navigate(['/themes']);
+        this.isLoading = false;
       },
       (error: HttpErrorResponse) => {
         this.errorMessage = error.error.message;
